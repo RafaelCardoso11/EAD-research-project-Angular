818368375fc176208b21b6ed30c8da6c3e2ff945
André Rafael Cardoso Reis <rafabatera-8@hotmail.com>
2021-05-27 17:25:55 -0300

    gitIgnore

--- src/app/services/form.service.ts
+++ src/app/services/form.service.ts
@@ -2,19 +2,19 @@ import { Usuario } from '../components/templates/Forms/usuario-form-model';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+const { baseUrl } = require('./.env')
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormCreateService {
-  baseUrl = "http://localhost:3001/usuarios";
 
   constructor(private http: HttpClient ) { }
 
   newForm(usuario: Usuario): Observable<Usuario>{
-   return this.http.post<Usuario>(this.baseUrl, usuario)
+   return this.http.post<Usuario>(baseUrl, usuario)
   }
   lenghtUsuarios(usuario: Usuario): Observable<Usuario>{
-    return this.http.get(this.baseUrl)
+    return this.http.get(baseUrl)
   }
 }

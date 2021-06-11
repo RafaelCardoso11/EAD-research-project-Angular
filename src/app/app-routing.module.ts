f319a51cb43f8dee119ffc1e3a2aa393350a1621
Rafael <Rafabatera-8@hotmail.com>
2021-06-11 18:05:13 -0300

    Alterações gerais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,8 @@
 
-
-
-import { PageHomeComponent } from './components/page-home/page-home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: "",
-    component: PageHomeComponent
-  }
   
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HeaderComponent } from './components/templates/header/header.component'
 import { FooterComponent } from './components/templates/footer/footer.component';
 import { ContentHeaderViewComponent } from './components/viewTemplates/content-header-view/content-header-view.component';
 import { ContentMoreAboutResearchViewComponent } from './components/viewTemplates/content-more-about-research-view/content-more-about-research-view.component';
-import { PageHomeComponent } from './components/page-home/page-home.component';
 import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
@@ -18,8 +17,7 @@ import {FormsModule} from '@angular/forms';
     HeaderComponent,
     FooterComponent,
     ContentHeaderViewComponent,
-    ContentMoreAboutResearchViewComponent,
-    PageHomeComponent
+    ContentMoreAboutResearchViewComponent
   ],
   imports: [
     BrowserModule,
